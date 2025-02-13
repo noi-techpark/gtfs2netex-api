9aa42d2968d61457943ac20c2d76e0d24b0ac063
Clemens Zagler <c.zagler@noi.bz.it>
2025-02-13 16:31:44 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Small API wrapping https://github.com/noi-techpark/GTFS2NeTEx-converter with a REST call.
+
+Make sure to have checked out the subrepo, or the docker build will be incomplete
---
+++ src/main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"compress/gzip"
+	"fmt"
+	"io"
+	"log"
+	"mime/multipart"
+	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
+
+	"github.com/evilsocket/islazy/zip"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+type ConvertReq struct {
+	Nuts    string                `form:"nuts" binding:"required"`
+	Vat     string                `form:"vat" binding:"required"`
+	Version string                `form:"version" binding:"required"`
+	Az      string                `form:"az" binding:"required"`
+	File    *multipart.FileHeader `form:"file" binding:"required"`
+}
+
+func main() {
+	g := gin.New()
+
+	g.Use(gin.Recovery())
+	g.Use(cors.Default())
+
+	g.POST("/", func(ctx *gin.Context) {
+		var recObj ConvertReq
+
+		if err := ctx.ShouldBind(&recObj); err != nil {
+			ctx.String(http.StatusBadRequest, fmt.Sprintf("err: %s", err.Error()))
+			return
+		}
+
+		workdir, err := os.MkdirTemp("", "gtfs2netex.")
+		if err != nil {
+			log.Panic("Could not create temp directory")
+		}
+		defer os.RemoveAll(workdir)
+
+		gtfs := filepath.Join(workdir, "in.gtfs")
+		if err := ctx.SaveUploadedFile(recObj.File, gtfs); err != nil {
+			log.Panic(err)
+		}
+
+		_, err = zip.Unzip(gtfs, workdir)
+		if err != nil {
+			log.Panic(err)
+		}
+
+		// launch the external converter tool
+		cmd := exec.Command("python3", os.Args[1], "--folder", workdir, "--db", "tmp", "--NUTS", recObj.Nuts, "--vat", recObj.Vat, "--version", recObj.Version, "--az", recObj.Az)
+		output, err := cmd.Output()
+		if err != nil {
+			log.Panic(err)
+		}
+		fmt.Print(string(output))
+
+		e, _ := os.ReadDir(workdir)
+		for _, e := range e {
+			fmt.Println(e)
+		}
+
+		matches, err := filepath.Glob(filepath.Join(workdir, "*.xml.gz"))
+		if err != nil {
+			log.Panic(err)
+		}
+
+		if len(matches) != 1 {
+			log.Panic("Could not find output file. Check stdout output,  maybe the converter failed without setting exit code", matches)
+		}
+
+		f, err := os.Open(matches[0])
+		if err != nil {
+			log.Panic(err)
+		}
+
+		gz, err := gzip.NewReader(f)
+		if err != nil {
+			log.Panic(err)
+		}
+		defer gz.Close()
+
+		unzipped, err := io.ReadAll(gz)
+		if err != nil {
+			log.Panic(err)
+		}
+
+		ctx.Data(http.StatusOK, "application/xml", unzipped)
+	})
+
+	g.Run()
+}
